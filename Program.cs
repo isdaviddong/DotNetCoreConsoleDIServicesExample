a9819200c144ddcdbf2f44ef67bc0f450e43d09a
David Tung <isdaviddong@gmail.com>
2023-04-18 16:15:49 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace consoleDI
 {
+    // 注意，他不是 Main
     public class MainApp
     {
         private readonly ISalaryFormula _SalaryFormula;
@@ -11,30 +12,36 @@ namespace consoleDI
             _SalaryFormula = SalaryFormula;
         }
  
+        //注意，這裡不是 Console 程式的進入點 Main
         public void Main()
         {
             //一般員工 SalaryFormula
             SalaryCalculator SC = new SalaryCalculator(_SalaryFormula);
             //注意參數完全相同
             float amount = SC.Calculate(8 * 19, 200, 8);
-            Console.Write("\nSalaryFormula--->amount:" + amount);
+            Console.WriteLine("\nSalaryFormula--->amount:" + amount);
         }
     }
 
+    //這裡才是真正的進入點 main 的類別 Program
     public class Program
     {
-
+        //這裡才是真正的進入點 main
         static void Main(string[] args)
         {
-            //build DI Services
+            //建立 DI Services
             var serviceCollection = new Microsoft.Extensions.DependencyInjection.ServiceCollection();
+            //註冊 DI物件 (為了未來的注入)
             serviceCollection.AddTransient<MainApp>();
             serviceCollection.AddTransient<ISalaryFormula, SalaryFormula>();
-            //serviceCollection.AddTransient<ISalaryFormula, BossSalaryFormula>();
-            var serviceProvider = serviceCollection.BuildServiceProvider();
-            //run MainApp.Main with DI
-            serviceProvider.GetRequiredService<MainApp>().Main();
+            //serviceCollection.AddTransient<ISalaryFormula, BossSalaryFormula>(); //先槓掉
             
+            //建立 DI Provider
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            //執行 MainApp，這裡會自動注入 ISalaryFormula
+            serviceProvider.GetRequiredService<MainApp>().Main();
+
+            Console.WriteLine("press any key to exit...");
             Console.ReadKey();
         }
     }
